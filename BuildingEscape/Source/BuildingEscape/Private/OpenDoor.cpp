d2e344639be56eb371565e97ce7eb81403463876
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2019-12-16 23:26:55 -0500

    09 - Using Linear Interpolation

--- BuildingEscape/Source/BuildingEscape/Private/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/Private/OpenDoor.cpp
@@ -20,9 +20,8 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FRotator Rotation = FRotator(0.0f, 90.0f, 0.0f);
-	GetOwner()->SetActorRotation(Rotation);
-	
+	TargetYaw = 90.0f;
+
 }
 
 
@@ -31,6 +30,13 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f "), GetOwner()->GetActorRotation().Yaw);
+
+	float StartingYaw = GetOwner()->GetActorRotation().Yaw;
+	FRotator OpenDoor = FRotator(0.0f, TargetYaw, 0.0f);
+	OpenDoor.Yaw = FMath::Lerp(StartingYaw, TargetYaw, 0.1f);
+	GetOwner()->SetActorRotation(OpenDoor);
+
+
 }
 
